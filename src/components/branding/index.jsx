4e590b846622991a6de50e42729059dbab51c634
Chris Shim <94628616+ChrisShim98@users.noreply.github.com>
2023-01-15 16:05:45 -0500

    added icon personality

--- src/components/branding/index.jsx
+++ src/components/branding/index.jsx
@@ -6,13 +6,13 @@ import {TbCurrencyDollar} from 'react-icons/tb'
 
 const Branding = () => {
   return (
-    <div className='bg-gray-100 w-screen min-h-[75vh] branding-container text-center pointer-events-none'>
+    <div className='bg-gray-100 w-screen min-h-[75vh] branding-container text-center'>
         <div className='w-full min-h-[75vh] flex flex-col lg:flex-row py-20 lg:py-0 place-content-center place-items-center gap-4 lg:gap-6'>
             <div className='w-[75%] lg:w-[25%] flex flex-col place-content-center place-items-center'>
-                <div className='border-2 rounded-full border-gray-300 w-20 h-20 flex place-content-center place-items-center'>
+                <div className='border-2 hover:bg-[#9a43c4] hover:text-white rounded-full border-gray-300 w-20 h-20 flex place-content-center place-items-center'>
                     <BsPlay className='ml-1' size={'10rem'}/>
                 </div>
-                <div className='pt-4'>
+                <div className='pt-4 pointer-events-none'>
                     <h1 className='font-bold text-2xl'>Top Songs</h1>
                     <p className='pt-4 text-sm'>
                         Our music has been signed and produced by the biggest names in the industry
@@ -21,10 +21,10 @@ const Branding = () => {
                 </div>
             </div>
             <div className='w-[75%] lg:w-[25%] flex flex-col place-content-center place-items-center'>
-                <div className='border-2 rounded-full border-gray-300 w-20 h-20 flex place-content-center place-items-center'>
+                <div className='border-2 hover:bg-[#9a43c4] hover:text-white rounded-full border-gray-300 w-20 h-20 flex place-content-center place-items-center'>
                     <FiMusic className='mr-1' size={'3rem'}/>
                 </div>
-                <div className='pt-4'>
+                <div className='pt-4 pointer-events-none'>
                     <h1 className='font-bold text-2xl'>Internationally Acclaimed</h1>
                     <p className='pt-4 text-sm'>
                         Our music has been signed and produced by the biggest names in the industry
@@ -33,10 +33,10 @@ const Branding = () => {
                 </div>
             </div>
             <div className='w-[75%] lg:w-[25%] flex flex-col place-content-center place-items-center'>
-                <div className='border-2 rounded-full border-gray-300 w-20 h-20 flex place-content-center place-items-center'>
+                <div className='border-2 hover:bg-[#9a43c4] hover:text-white rounded-full border-gray-300 w-20 h-20 flex place-content-center place-items-center'>
                     <TbCurrencyDollar size={'3rem'}/>
                 </div>
-                <div className='pt-4'>
+                <div className='pt-4 pointer-events-none'>
                     <h1 className='font-bold text-2xl'>Affordable</h1>
                     <p className='pt-4 text-sm'>
                         Our music comes at the best quality while still being affordable and fits
